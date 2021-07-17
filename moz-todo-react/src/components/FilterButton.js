5ad5b91ea8fd1ad4427f87e96f15020bb25353c5
William Summy <willsummy814@gmail.com>
2021-07-17 11:50:20 -0400

    Created a FilterButton componenet
    -To save on redundancies, a FilterButton component was created
    -This is then imported into App.js for use instead of define all 3 separate Forms themselves
    -This takes in props to then use for its distinct attributes/texts

---
+++ moz-todo-react/src/components/FilterButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function FilterButton(props) {
+    return (
+        <button type="button" className="btn toggle-btn" aria-pressed={props.ariaPressed}>
+          <span className="visually-hidden">Show </span>
+          <span>{props.name}</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+    );
+}
