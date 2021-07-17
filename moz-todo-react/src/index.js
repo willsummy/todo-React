343063b63609a5d6b9648be450656515a17787de
William Summy <willsummy814@gmail.com>
2021-07-16 20:07:54 -0400

    React props
    -Declaring a prop inside of <App /> acts like an argument call to the App function in App.js

--- moz-todo-react/src/index.js
+++ moz-todo-react/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+// this was possible because App.js exported itself
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App subject="Clarice" />
   </React.StrictMode>,
   document.getElementById('root')
 );
